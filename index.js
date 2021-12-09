16e8885568ffb70cc561ddc9bc1054d9130485eb
ZachLayman <zlayman@oakland.edu>
2021-12-09 13:17:25 -0500

    side nav

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const app = express();
 
 
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000
 
-// Static Files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/imgs', express.static(__dirname + 'public/imgs'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/html', express.static(__dirname + 'public/html'))
-
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-app.use(express.static('public'));
+
