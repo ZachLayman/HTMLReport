bc3d878d1b5248a4cad92d544e0233eaf89d372e
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:06:00 -0500

    changes

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-    res.render("index");
-  });
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 app.use(express.static('public'));
