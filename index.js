532ec9c495d878f0df9250ac6679cfdae3bd59d5
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:26:47 -0500

    routing

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express();
 
-if (process.env.NODE_ENV === "production"{
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
-  
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('public/html'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'public/html', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 app.use(express.static('public'));
