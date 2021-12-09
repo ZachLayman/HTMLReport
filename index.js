4edeffa82f2ae504ae8026002e7d9766ff1fdf74
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:23:15 -0500

    .json

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express();
 
+if (process.env.NODE_ENV === "production"{
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+  
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 app.use(express.static('public'));
