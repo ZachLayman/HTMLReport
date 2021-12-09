0d1b0f7bdd4b33c37446b9e479acd01033cd0fa5
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:29:45 -0500

    removed

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const express = require('express')
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('public/html'));
-}
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'public/html', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
