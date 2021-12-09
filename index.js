6497faf9b03eada4e773079a0e02f36e63a50996
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:53:12 -0500

    routing

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/imgs', express.static(__dirname + 'public/imgs'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/html', express.static(__dirname + 'public/html'))
+
 app.get("/public", (req, res) => {
     res.send("hello world")
 })
