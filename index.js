ce1fce1f2a14230462aa466fbe65cb5770f52c60
ZachLayman <zlayman@oakland.edu>
2021-12-08 21:55:34 -0500

    routing

--- index.js
+++ index.js
@@ -11,8 +11,5 @@ app.use('/imgs', express.static(__dirname + 'public/imgs'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/html', express.static(__dirname + 'public/html'))
 
-app.get("/public", (req, res) => {
-    res.send("hello world")
-})
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 app.use(express.static('public'));
