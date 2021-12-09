b666487fe33189bb5225d05ee3d64b36a6ca2224
ZachLayman <zlayman@oakland.edu>
2021-12-08 20:52:11 -0500

    heroku push

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 app.use(express.static('public'));
